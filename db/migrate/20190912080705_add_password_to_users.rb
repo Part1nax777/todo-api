7b34c12f2fd39f85a658841683c684931444d71f
Part1nax777 <serewka-ru@yandex.ru>
2019-09-12 15:29:31 +0300

    correct model user

--- db/migrate/20190912080705_add_password_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddPasswordToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :password, :string, null: false
-    add_column :users, :password_confirmation, :string, null: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_080705) do
+ActiveRecord::Schema.define(version: 2019_09_11_073001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,11 +34,10 @@ ActiveRecord::Schema.define(version: 2019_09_12_080705) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password", null: false
-    t.string "password_confirmation", null: false
   end
 
   add_foreign_key "items", "todos"
